05e1a84ada325ebbee83aa68881a06eef6ec70a8
k21066-Shimizu <k21066kk@aitech.ac.jp>
2022-08-09 14:28:18 +0900

    座標データファイルの分割

--- frontend/src/js/ait-map.js
+++ frontend/src/js/ait-map.js
@@ -1,3 +1,6 @@
+import points from '../data/points.json';
+import loads from '../data/loads.json';
+
 const DISPLAY_RANGE = [
     [35.18760, 137.103485],
     [35.17910, 137.125330]
@@ -67,51 +70,5 @@ const displayData = (map) => {
         L.polyline(path, { color: "red", weight: 5, opacity: 0.5 }).addTo(map);
     }
 }
-
-// TODO いずれファイル分割する
-const points = {
-    "MainGate": {
-        "jp": "正門",
-        "coordinate": [35.18186301683754, 137.10961639881137]
-    },
-    "BusStopMainGate": {
-        "jp": "バス停(正門)",
-        "coordinate": [35.18225761957009, 137.10985243320468]
-    },
-    "BusStopDormitoryFront": {
-        "jp": "バス停(合宿寮前)",
-        "coordinate": [35.182814444611296, 137.11092531681064]
-    },
-    "BuildingNew1Front": {
-        "jp": "新1号館前",
-        "coordinate": [35.18369571109226, 137.11085557937625]
-    },
-    "HeadquartersBuilding": {
-        "jp": "本部棟",
-        "coordinate": [35.183090663491726, 137.11204111576083]
-    }
-};
-
-const loads = [
-    {
-        "start": "MainGate",
-        "end": "BusStopMainGate",
-        "via": [[35.18215239236206, 137.10972368717196]]
-    },
-    {
-        "start": "BusStopMainGate",
-        "end": "BusStopDormitoryFront",
-        "via": [[35.18256891592947, 137.11022257804873], [35.18273552475875, 137.11026012897494]]
-    },
-    {
-        "start": "BusStopDormitoryFront",
-        "end": "BuildingNew1Front"
-    },
-    {
-        "start": "BusStopDormitoryFront",
-        "end": "HeadquartersBuilding",
-        "via": [[35.182840751211785, 137.11113989353183], [35.182836366778965, 137.11178898811343], [35.18293720867404, 137.11223959922793], [35.18311258558489, 137.11222887039187]]
-    }
-];
 
 main();
