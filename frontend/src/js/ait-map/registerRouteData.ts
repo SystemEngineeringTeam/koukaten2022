a4dd4d9e6956f905abd35b22cfc58408d56d94f6
k21066-Shimizu <k21066kk@aitech.ac.jp>
2022-08-10 19:52:39 +0900

    マーカーの選択と表示名設定機能の追加

--- frontend/src/js/ait-map/registerRouteData.ts
+++ frontend/src/js/ait-map/registerRouteData.ts
@@ -12,17 +12,22 @@ const outputField = document.querySelector<HTMLDivElement>('#output');
 const dataManager = new DataManager(routeData);
 
 const registerRouteData = (map: L.Map) => {
+    for (const point of routeData.points) {
+        pointMarker(point).addTo(map);
+    }
+
     map.on('click', (e) => {
         const settings = getSettings();
         switch (settings.mode) {
-            case 0:
+            case 0: {
                 const data = {
                     id: DataManager.getId(),
-                    jp: settings.displayName,
+                    jp: "",
                     latlng: e.latlng
                 };
                 pointMarker(data).addTo(map);
                 break;
+            }
             default:
                 break;
         }
@@ -36,12 +41,19 @@ const registerRouteData = (map: L.Map) => {
 };
 
 const pointMarker = (data: PointData) => {
-    const marker = L.marker(data.latlng, { draggable: true });
+    const marker = L.marker(data.latlng, { draggable: true, opacity: 0.5 });
+    const updateSelected = updateSelectedGene(marker, data);
     marker.on('moveend', (e) => {
+        updateSelected()
         data.latlng = marker.getLatLng();
         dataManager.add('point', data);
     });
 
+    marker.on('click', (e) => {
+        updateSelected();
+    });
+
+    updateSelected();
     dataManager.add('point', data);
     return marker;
 };
@@ -53,5 +65,21 @@ const getSettings = () => {
 
     return { mode, isAutoConnect, displayName };
 };
+
+let selected: L.Marker | undefined;
+const updateSelectedGene = (marker: L.Marker, data: PointData) => () => {
+    selected?.setOpacity(0.5);
+    marker.setOpacity(1);
+
+    selected = marker;
+
+    if (displayNameElm) {
+        displayNameElm.value = data.jp ?? "";
+        displayNameElm.onchange = (e) => {
+            data.jp = displayNameElm.value;
+            dataManager.add('point', data);
+        }
+    }
+}
 
 export default registerRouteData;
