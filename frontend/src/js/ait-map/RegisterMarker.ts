d9ed74a07da04abc25a751d92038da0319e2f7b2
k21066-Shimizu <k21066kk@aitech.ac.jp>
2022-08-24 18:27:41 +0900

    pointの削除

--- frontend/src/js/ait-map/RegisterMarker.ts
+++ frontend/src/js/ait-map/RegisterMarker.ts
@@ -8,14 +8,16 @@ const PATH_DB = new Map<string, PathData>();
 const OPACITY = { UNSELECT: 0.5, SELECT: 1 };
 
 class RegisterMarker {
-    point: PointData;
+    id: string;
+    jp: string | undefined;
     marker: L.Marker;
     reach: Map<string, Path>;
 
     static selected: RegisterMarker | undefined;
 
     constructor (map: L.Map, point: PointData) {
-        this.point = point;
+        this.id = point.id;
+        this.jp = point.jp;
         this.marker = L.marker(point.latlng, { draggable: true });
         this.reach = new Map();
 
@@ -40,6 +42,18 @@ class RegisterMarker {
                 const line = this.connectTo(RegisterMarker.selected);
                 console.log(line);
                 line?.addTo(map);
+            } else if (settings.mode === 2) {
+                this.select(false);
+                for (const path of this.reach.values()) {
+                    for (const point of path.via) {
+                        point.reach.delete(this.id);
+                    }
+                    path.line.remove();
+                    PATH_DB.delete(path.id);
+                }
+                this.marker.remove();
+                POINT_DB.delete(this.id);
+                return;
             }
 
             if (this.marker === RegisterMarker.selected?.marker) {
@@ -55,24 +69,31 @@ class RegisterMarker {
     }
 
     setPoint (point: Partial<PointData>) {
-        Object.assign(this.point, point);
-        POINT_DB.set(this.point.id, this.point);
+        this.id = point.id ?? this.id;
+        this.jp = point.jp ?? this.jp;
+        const latlng = this.marker.getLatLng();
+        if (point.latlng !== undefined && !latlng.equals(point.latlng)) {
+            this.marker.setLatLng(point.latlng);
+        }
+        POINT_DB.set(this.id, {
+            latlng, id: this.id, jp: this.jp
+        });
     }
 
     select (selectThis: boolean) {
         RegisterMarker.selected?.marker.setOpacity(OPACITY.UNSELECT);
         if (selectThis) this.marker.setOpacity(OPACITY.SELECT);
-        displayNameElm.value = selectThis ? this.point.jp ?? "" : "";
+        displayNameElm.value = selectThis ? this.jp ?? "" : "";
         RegisterMarker.selected = selectThis ? this : undefined;
     }
 
     connectTo (to: RegisterMarker) {
         const from: RegisterMarker = this;
-        if (from.reach.has(to.point.id)) return;
+        if (from.reach.has(to.id)) return;
         const path = new Path(from, to);
 
-        from.reach.set(to.point.id, path);
-        to.reach.set(from.point.id, path);
+        from.reach.set(to.id, path);
+        to.reach.set(from.id, path);
         return path.line;
     }
 
@@ -97,9 +118,10 @@ class Path {
         this.id = Path.getId();
         this.via = via;
         this.line = L.polyline(
-            this.getLatlngs(),
+            [],
             { color: 'green', opacity: 0.5 }
         );
+        this.update();
     }
 
     setVia (...via: RegisterMarker[]) {
@@ -107,11 +129,29 @@ class Path {
     }
 
     update () {
+        PATH_DB.set(this.id, {
+            from: this.via[0].id,
+            to: this.via[1].id,
+            distance: this.getDistance()
+        });
         this.line.setLatLngs(this.getLatlngs());
     }
 
     getLatlngs () {
         return this.via.map(v => v.marker.getLatLng());
+    }
+
+    getDistance () {
+        let distance = 0;
+        let prev: undefined | L.LatLng;
+        for (const curr of this.getLatlngs()) {
+            if (prev === undefined) {
+                prev = curr;
+                continue;
+            }
+            distance += Math.sqrt((prev.lat - curr.lat) ** 2 + (prev.lng - curr.lng) ** 2);
+        }
+        return distance;
     }
 
     static getId () {
