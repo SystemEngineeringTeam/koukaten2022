4a8f17baaaa34be208cba2489535f4185543377a
Emma Paris <yusei4520030401@gmail.com>
2022-09-07 15:27:20 +0900

    Revert "整理"
    This reverts commit 76be3986afa6568d3cc9c48c3a1779d5619825c1.

--- backend/model/db/subject_model.go
+++ backend/model/db/subject_model.go
@@ -5,30 +5,30 @@ import (
 )
 
 type Building struct {
-	BuildingName string `json:"building_name"`
-	Latitude     string `json:"latitude"`
-	Longitude    string `json:"longitude"`
+	Building_name string `json:"building_name"`
+	Latitude      string `json:"latitude"`
+	Longitude     string `json:"longitude"`
 }
 
 // Subject テーブル情報
 
 type Class_room struct {
-	Roomnumber   string `json:"room_number"`
-	RoomName     string `json:"room_name"`
-	BuildingName string `json:"building_name"`
-	Floor        int    `json:"floor"`
+	Room_number   string `json:"room_number"`
+	Room_name     string `json:"room_name"`
+	Building_name string `json:"building_name"`
+	Floor         int    `json:"floor"`
 }
 
 type Subject struct {
 	Id             int
-	FolderName     string `json:"folder_name"`
-	SubjectNode    string `json:"subject_code"`
-	SubjectName    string `json:"subject_name"`
+	Folder_name    string `json:"folder_name"`
+	Subject_code   string `json:"subject_code"`
+	Subject_name   string `json:"subject_name"`
 	Grade          string `json:"grade"`
-	ClassNode      string `json:"class_code"`
-	ClassRoom      string `json:"class_room"`
+	Class_code     string `json:"class_code"`
+	Class_room     string `json:"class_room"`
 	Semester       string `json:"semester"`
-	DayTime        string `json:"day_time"`
+	Day_time       string `json:"day_time"`
 	Classification string `json:"classification"`
 	Credit         int    `json:"credit"`
 }
