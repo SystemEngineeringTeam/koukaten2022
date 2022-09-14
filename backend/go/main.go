6ae350d4d3bfa433d8bc830df4161ffc27608074
keisuke <88765310+melon0658@users.noreply.github.com>
2022-09-14 22:18:56 +0900

    Revert "echoでwebサーバの構築"

--- backend/go/main.go
+++ backend/go/main.go
@@ -1,43 +1,13 @@
 package main
 
 import (
-	"html/template"
-	"io"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	DataSets "go/search/model/data_sets"
+	//Student "go/search/model/student"
+	"fmt"
 )
 
-type Template struct {
-	templates *template.Template
-}
-
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
+//動作確認
 func main() {
-
-	t := &Template{
-		templates: template.Must(template.ParseGlob("../../*.html")),
-	}
-
-	//インスタンスの作成
-	e := echo.New()
-
-	e.Renderer = t
-
-	//ミドルウェアを設定
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	//ルートを設定
-	e.GET("/", Start)
-
-	//サーバをポート番号8000で起動
-	e.Logger.Fatal(e.Start(":8000"))
-}
-
-func Start(c echo.Context) error {
-	return c.File("../../＊.html")
+	d := DataSets.GetBuildingBuildingName("1号")
+	fmt.Println(d[2])
 }
