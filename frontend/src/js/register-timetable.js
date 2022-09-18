f14ecd8c32ef1b1287249c1824c77a996b5f7604
k21060 <k21060kk@aitech.ac.jp>
2022-09-18 16:23:15 +0900

    id重複しないようにした

--- frontend/src/js/register-timetable.js
+++ frontend/src/js/register-timetable.js
@@ -1,3 +1,138 @@
-document.getElementById("button").onclick = function(){
+document.getElementById("monⅠbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("tueⅠbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("wedⅠbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("thuⅠbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("friⅠbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("satⅠbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+
+
+document.getElementById("monⅡbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("tueⅡbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("wedⅡbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("thuⅡbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("friⅡbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("satⅡbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+
+
+document.getElementById("monⅢbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("tueⅢbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("wedⅢbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("thuⅢbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("friⅢbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("satⅢbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+
+
+document.getElementById("monⅣbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("tueⅣbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("wedⅣbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("thuⅣbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("friⅣbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("satⅣbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+
+
+document.getElementById("monⅤbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("tueⅤbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("wedⅤbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("thuⅤbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("friⅤbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("satⅤbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+
+
+document.getElementById("monⅥbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("tueⅥbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("wedⅥbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("thuⅥbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("friⅥbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("satⅥbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+
+
+document.getElementById("monⅦbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("tueⅦbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("wedⅦbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("thuⅦbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("friⅦbutton").onclick = function(){
+    window.open('chooseSubject.html', '_blank', 'noreferrer');
+}
+document.getElementById("satⅦbutton").onclick = function(){
     window.open('chooseSubject.html', '_blank', 'noreferrer');
 }
